78c01759d06cb84689713bbc93ff1b1f2f342f8f
ftrixy <dmitry.kravetss@gmail.com>
2023-05-25 15:45:51 +0300

    50% complete

---
+++ app/js/main.js
@@ -0,0 +1,31 @@
+const anchors = document.querySelectorAll('a[href*="#"]'); 
+
+for (let anchor of anchors) {
+    anchor.addEventListener("click", function (event) {
+        event.preventDefault();
+
+        const blockID = anchor.getAttribute('href')
+
+        document.querySelector('' + blockID).scrollIntoView({
+            behavior: "smooth",
+            block: "start"
+        })
+    })
+}
+
+
+
+
+function onEntry(entry) {
+    entry.forEach(change => {
+      if (change.isIntersecting) {
+        change.target.classList.add('element-show');
+      }
+    });
+  }
+  let options = { threshold: [0.5] };
+  let observer = new IntersectionObserver(onEntry, options);
+  let elements = document.querySelectorAll('.about__inner');
+  for (let elm of elements) {
+    observer.observe(elm);
+  }
